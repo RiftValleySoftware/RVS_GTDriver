7640e6b7778e0a588708f11831099b9ff52969ab
Chris Marshall <chris@riftvalleysoftware.com>
2019-09-12 09:48:28 -0400

    Initial Commit

---
+++ RVS_GTDriver/RVS_GTDriver.h
@@ -0,0 +1,19 @@
+//
+//  RVS_GTDriver.h
+//  RVS_GTDriver
+//
+//  Created by Chris Marshall on 9/12/19.
+//  Copyright © 2019 The Great Rift Valley Software Company. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RVS_GTDriver.
+FOUNDATION_EXPORT double RVS_GTDriverVersionNumber;
+
+//! Project version string for RVS_GTDriver.
+FOUNDATION_EXPORT const unsigned char RVS_GTDriverVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RVS_GTDriver/PublicHeader.h>
+
+
