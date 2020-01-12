60a109b909978089ec71fe19fd2017158237b032
Chris Marshall <chris@riftvalleysoftware.com>
2020-01-12 09:03:04 -0500

    Rearranged the project to be a bit more sensible. Genericized most of the tests. Added a (still not working) tvOS test.

--- RVS_BTDriver_Tests/RVS_BTDriver_iOS_Tests_TestPropertyClass.swift
+++ RVS_BTDriver_Tests/RVS_BTDriver_iOS_Tests_TestPropertyClass.swift
@@ -27,13 +27,13 @@ import XCTest
 /* ###################################################################################################################################### */
 /**
  */
-class RVS_BTDriver_iOS_Tests_TestPropertyClass: XCTestCase {
+class RVS_BTDriver_Tests_TestPropertyClass: XCTestCase {
     /* ################################################################## */
     /**
      Test Integer zero
      */
     func testPropertyDataTypeZero() {
-        let testTargetInstance = RVS_BTDriver_iOS_Tests_Property_Int_Zero()
+        let testTargetInstance = RVS_BTDriver_Tests_Property_Int_Zero()
         let testTarget = testTargetInstance.value
         let comparisonTarget = RVS_BTDriver_PropertyProtocol_Type_Enum.intValue(0)
         XCTAssertEqual(testTarget, comparisonTarget, "Values Don't Match!")
@@ -49,7 +49,7 @@ class RVS_BTDriver_iOS_Tests_TestPropertyClass: XCTestCase {
      Test Integer zero (decimal)
      */
     func testPropertyDataTypeZeroDecimal() {
-        let testTargetInstance = RVS_BTDriver_iOS_Tests_Property_Int_ZeroDecimal()
+        let testTargetInstance = RVS_BTDriver_Tests_Property_Int_ZeroDecimal()
         let testTarget = testTargetInstance.value
         let comparisonTarget = RVS_BTDriver_PropertyProtocol_Type_Enum.intValue(0)
         XCTAssertEqual(testTarget, comparisonTarget, "Values Don't Match!")
@@ -65,7 +65,7 @@ class RVS_BTDriver_iOS_Tests_TestPropertyClass: XCTestCase {
      Test highest Int (Int32.max)
      */
     func testPropertyDataTypeMax() {
-        let testTargetInstance = RVS_BTDriver_iOS_Tests_Property_Int_MaxInt()
+        let testTargetInstance = RVS_BTDriver_Tests_Property_Int_MaxInt()
         let testTarget = testTargetInstance.value
         let comparisonTarget = RVS_BTDriver_PropertyProtocol_Type_Enum.intValue(Int(Int32.max))
         XCTAssertEqual(testTarget, comparisonTarget, "Values Don't Match!")
@@ -81,7 +81,7 @@ class RVS_BTDriver_iOS_Tests_TestPropertyClass: XCTestCase {
      Test lowest Int (Int32.min)
      */
     func testPropertyDataTypeMin() {
-        let testTargetInstance = RVS_BTDriver_iOS_Tests_Property_Int_MinInt()
+        let testTargetInstance = RVS_BTDriver_Tests_Property_Int_MinInt()
         let testTarget = testTargetInstance.value
         let comparisonTarget = RVS_BTDriver_PropertyProtocol_Type_Enum.intValue(Int(Int32.min))
         XCTAssertEqual(testTarget, comparisonTarget, "Values Don't Match!")
@@ -97,7 +97,7 @@ class RVS_BTDriver_iOS_Tests_TestPropertyClass: XCTestCase {
      Make sure that a string containing a decimal number, where the decimal is .0000, is returned as an Int.
      */
     func testPropertyDataTypeWithDecimal() {
-        let testTargetInstance = RVS_BTDriver_iOS_Tests_Property_IntWithDecimal()
+        let testTargetInstance = RVS_BTDriver_Tests_Property_IntWithDecimal()
         let testTarget = testTargetInstance.value
         let comparisonTarget = RVS_BTDriver_PropertyProtocol_Type_Enum.intValue(1)
         XCTAssertEqual(testTarget, comparisonTarget, "Values Don't Match!")
@@ -113,7 +113,7 @@ class RVS_BTDriver_iOS_Tests_TestPropertyClass: XCTestCase {
      Make sure that a string containing a decimal number, where the decimal is .0000, is returned as an Int (Negative).
      */
     func testPropertyDataTypeWithDecimalNegative() {
-        let testTargetInstance = RVS_BTDriver_iOS_Tests_Property_NegativeIntWithDecimal()
+        let testTargetInstance = RVS_BTDriver_Tests_Property_NegativeIntWithDecimal()
         let testTarget = testTargetInstance.value
         let comparisonTarget = RVS_BTDriver_PropertyProtocol_Type_Enum.intValue(-1)
         XCTAssertEqual(testTarget, comparisonTarget, "Values Don't Match!")
@@ -129,7 +129,7 @@ class RVS_BTDriver_iOS_Tests_TestPropertyClass: XCTestCase {
      Make sure that positive Doubles are returned.
      */
     func testPropertyDataTypeSimpleFloat() {
-        let testTargetInstance = RVS_BTDriver_iOS_Tests_Property_SimpleFloat()
+        let testTargetInstance = RVS_BTDriver_Tests_Property_SimpleFloat()
         let testTarget = testTargetInstance.value
         let comparisonTarget = RVS_BTDriver_PropertyProtocol_Type_Enum.floatValue(1.2345)
         XCTAssertEqual(testTarget, comparisonTarget, "Values Don't Match!")
@@ -145,7 +145,7 @@ class RVS_BTDriver_iOS_Tests_TestPropertyClass: XCTestCase {
      Make sure that negative Doubles are returned.
      */
     func testPropertyDataTypeSimpleNegativeFloat() {
-        let testTargetInstance = RVS_BTDriver_iOS_Tests_Property_SimpleNegativeFloat()
+        let testTargetInstance = RVS_BTDriver_Tests_Property_SimpleNegativeFloat()
         let testTarget = testTargetInstance.value
         let comparisonTarget = RVS_BTDriver_PropertyProtocol_Type_Enum.floatValue(-1.2345)
         XCTAssertEqual(testTarget, comparisonTarget, "Values Don't Match!")
@@ -161,7 +161,7 @@ class RVS_BTDriver_iOS_Tests_TestPropertyClass: XCTestCase {
      In this test, we ensure that values beyond Int32.max are returned as Double.
      */
     func testPropertyDataTypeMaxPlusOne() {
-        let testTargetInstance = RVS_BTDriver_iOS_Tests_Property_Int_MaxIntPlusOne()
+        let testTargetInstance = RVS_BTDriver_Tests_Property_Int_MaxIntPlusOne()
         let testTarget = testTargetInstance.value
         let comparisonTarget = RVS_BTDriver_PropertyProtocol_Type_Enum.floatValue(Double(Int32.max) + 1)
         XCTAssertEqual(testTarget, comparisonTarget, "Values Don't Match!")
@@ -177,7 +177,7 @@ class RVS_BTDriver_iOS_Tests_TestPropertyClass: XCTestCase {
      In this test, we ensure that values below Int32.min are returned as Double.
      */
     func testPropertyDataTypeMinMinusOne() {
-        let testTargetInstance = RVS_BTDriver_iOS_Tests_Property_Int_MinIntMinusOne()
+        let testTargetInstance = RVS_BTDriver_Tests_Property_Int_MinIntMinusOne()
         let testTarget = testTargetInstance.value
         let comparisonTarget = RVS_BTDriver_PropertyProtocol_Type_Enum.floatValue(Double(Int32.min) - 1)
         XCTAssertEqual(testTarget, comparisonTarget, "Values Don't Match!")
@@ -193,7 +193,7 @@ class RVS_BTDriver_iOS_Tests_TestPropertyClass: XCTestCase {
      This test forces the number to be a String, by prepending a space.
      */
     func testPropertyDataTypeMinMinusOneStringy() {
-        let testTargetInstance = RVS_BTDriver_iOS_Tests_Property_Int_MinIntMinusOneStringy()
+        let testTargetInstance = RVS_BTDriver_Tests_Property_Int_MinIntMinusOneStringy()
         let testTarget = testTargetInstance.value
         let comparisonTarget = RVS_BTDriver_PropertyProtocol_Type_Enum.stringValue(" " + String(Double(Int32.min) - 1))
         XCTAssertEqual(testTarget, comparisonTarget, "Values Don't Match!")
@@ -209,7 +209,7 @@ class RVS_BTDriver_iOS_Tests_TestPropertyClass: XCTestCase {
      This does the same, by suffixing a space. It also is a String that could be an Int.
      */
     func testPropertyDataTypeMaxMinusOneStringy() {
-        let testTargetInstance = RVS_BTDriver_iOS_Tests_Property_Int_MaxIntMinusOneStringy()
+        let testTargetInstance = RVS_BTDriver_Tests_Property_Int_MaxIntMinusOneStringy()
         let testTarget = testTargetInstance.value
         let comparisonTarget = RVS_BTDriver_PropertyProtocol_Type_Enum.stringValue(String(Int32.max - 1) + " ")
         XCTAssertEqual(testTarget, comparisonTarget, "Values Don't Match!")
@@ -225,7 +225,7 @@ class RVS_BTDriver_iOS_Tests_TestPropertyClass: XCTestCase {
      This tests a pure string, with no numerical characters.
      */
     func testPropertyDataTypeString() {
-        let testTargetInstance = RVS_BTDriver_iOS_Tests_Property_String()
+        let testTargetInstance = RVS_BTDriver_Tests_Property_String()
         let testTarget = testTargetInstance.value
         let comparisonTarget = RVS_BTDriver_PropertyProtocol_Type_Enum.stringValue("Open the pod bay doors, Hal.")
         XCTAssertEqual(testTarget, comparisonTarget, "Values Don't Match!")
@@ -241,7 +241,7 @@ class RVS_BTDriver_iOS_Tests_TestPropertyClass: XCTestCase {
      This tests a pure string, in Japanese.
      */
     func testPropertyDataTypeStringJapanese() {
-        let testTargetInstance = RVS_BTDriver_iOS_Tests_Property_StringJapanese()
+        let testTargetInstance = RVS_BTDriver_Tests_Property_StringJapanese()
         let testTarget = testTargetInstance.value
         let comparisonTarget = RVS_BTDriver_PropertyProtocol_Type_Enum.stringValue("ポッドベイドア、Halを開きます。")
         XCTAssertEqual(testTarget, comparisonTarget, "Values Don't Match!")
@@ -258,7 +258,7 @@ class RVS_BTDriver_iOS_Tests_TestPropertyClass: XCTestCase {
 /* ###################################################################################################################################### */
 /**
  */
-class RVS_BTDriver_iOS_Tests_Property_Int_Zero: RVS_BTDriver_Property {
+class RVS_BTDriver_Tests_Property_Int_Zero: RVS_BTDriver_Property {
     override init() {
         super.init()
         rawValue = "0".data(using: .utf8)
@@ -270,7 +270,7 @@ class RVS_BTDriver_iOS_Tests_Property_Int_Zero: RVS_BTDriver_Property {
 /* ###################################################################################################################################### */
 /**
  */
-class RVS_BTDriver_iOS_Tests_Property_Int_ZeroDecimal: RVS_BTDriver_Property {
+class RVS_BTDriver_Tests_Property_Int_ZeroDecimal: RVS_BTDriver_Property {
     override init() {
         super.init()
         rawValue = "0.0000".data(using: .utf8)
@@ -282,7 +282,7 @@ class RVS_BTDriver_iOS_Tests_Property_Int_ZeroDecimal: RVS_BTDriver_Property {
 /* ###################################################################################################################################### */
 /**
  */
-class RVS_BTDriver_iOS_Tests_Property_Int_MaxInt: RVS_BTDriver_Property {
+class RVS_BTDriver_Tests_Property_Int_MaxInt: RVS_BTDriver_Property {
     override init() {
         super.init()
         rawValue = "\(Int32.max)".data(using: .utf8)
@@ -294,7 +294,7 @@ class RVS_BTDriver_iOS_Tests_Property_Int_MaxInt: RVS_BTDriver_Property {
 /* ###################################################################################################################################### */
 /**
  */
-class RVS_BTDriver_iOS_Tests_Property_Int_MinInt: RVS_BTDriver_Property {
+class RVS_BTDriver_Tests_Property_Int_MinInt: RVS_BTDriver_Property {
     override init() {
         super.init()
         rawValue = "\(Int32.min)".data(using: .utf8)
@@ -306,7 +306,7 @@ class RVS_BTDriver_iOS_Tests_Property_Int_MinInt: RVS_BTDriver_Property {
 /* ###################################################################################################################################### */
 /**
  */
-class RVS_BTDriver_iOS_Tests_Property_IntWithDecimal: RVS_BTDriver_Property {
+class RVS_BTDriver_Tests_Property_IntWithDecimal: RVS_BTDriver_Property {
     override init() {
         super.init()
         rawValue = "1.0000".data(using: .utf8)
@@ -318,7 +318,7 @@ class RVS_BTDriver_iOS_Tests_Property_IntWithDecimal: RVS_BTDriver_Property {
 /* ###################################################################################################################################### */
 /**
  */
-class RVS_BTDriver_iOS_Tests_Property_NegativeIntWithDecimal: RVS_BTDriver_Property {
+class RVS_BTDriver_Tests_Property_NegativeIntWithDecimal: RVS_BTDriver_Property {
     override init() {
         super.init()
         rawValue = "-1.0000".data(using: .utf8)
@@ -330,7 +330,7 @@ class RVS_BTDriver_iOS_Tests_Property_NegativeIntWithDecimal: RVS_BTDriver_Prope
 /* ###################################################################################################################################### */
 /**
  */
-class RVS_BTDriver_iOS_Tests_Property_SimpleFloat: RVS_BTDriver_Property {
+class RVS_BTDriver_Tests_Property_SimpleFloat: RVS_BTDriver_Property {
     override init() {
         super.init()
         rawValue = "1.2345".data(using: .utf8)
@@ -342,7 +342,7 @@ class RVS_BTDriver_iOS_Tests_Property_SimpleFloat: RVS_BTDriver_Property {
 /* ###################################################################################################################################### */
 /**
  */
-class RVS_BTDriver_iOS_Tests_Property_SimpleNegativeFloat: RVS_BTDriver_Property {
+class RVS_BTDriver_Tests_Property_SimpleNegativeFloat: RVS_BTDriver_Property {
     override init() {
         super.init()
         rawValue = "-1.2345".data(using: .utf8)
@@ -354,7 +354,7 @@ class RVS_BTDriver_iOS_Tests_Property_SimpleNegativeFloat: RVS_BTDriver_Property
 /* ###################################################################################################################################### */
 /**
  */
-class RVS_BTDriver_iOS_Tests_Property_Int_MaxIntPlusOne: RVS_BTDriver_Property {
+class RVS_BTDriver_Tests_Property_Int_MaxIntPlusOne: RVS_BTDriver_Property {
     override init() {
         super.init()
         let doubleVal = Double(Int32.max) + 1
@@ -367,7 +367,7 @@ class RVS_BTDriver_iOS_Tests_Property_Int_MaxIntPlusOne: RVS_BTDriver_Property {
 /* ###################################################################################################################################### */
 /**
  */
-class RVS_BTDriver_iOS_Tests_Property_Int_MinIntMinusOne: RVS_BTDriver_Property {
+class RVS_BTDriver_Tests_Property_Int_MinIntMinusOne: RVS_BTDriver_Property {
     override init() {
         super.init()
         let doubleVal = Double(Int32.min) - 1
@@ -380,7 +380,7 @@ class RVS_BTDriver_iOS_Tests_Property_Int_MinIntMinusOne: RVS_BTDriver_Property 
 /* ###################################################################################################################################### */
 /**
  */
-class RVS_BTDriver_iOS_Tests_Property_Int_MinIntMinusOneStringy: RVS_BTDriver_Property {
+class RVS_BTDriver_Tests_Property_Int_MinIntMinusOneStringy: RVS_BTDriver_Property {
     override init() {
         super.init()
         let doubleVal = Double(Int32.min) - 1
@@ -393,7 +393,7 @@ class RVS_BTDriver_iOS_Tests_Property_Int_MinIntMinusOneStringy: RVS_BTDriver_Pr
 /* ###################################################################################################################################### */
 /**
  */
-class RVS_BTDriver_iOS_Tests_Property_Int_MaxIntMinusOneStringy: RVS_BTDriver_Property {
+class RVS_BTDriver_Tests_Property_Int_MaxIntMinusOneStringy: RVS_BTDriver_Property {
     override init() {
         super.init()
         let val = Int32.max - 1
@@ -406,7 +406,7 @@ class RVS_BTDriver_iOS_Tests_Property_Int_MaxIntMinusOneStringy: RVS_BTDriver_Pr
 /* ###################################################################################################################################### */
 /**
  */
-class RVS_BTDriver_iOS_Tests_Property_String: RVS_BTDriver_Property {
+class RVS_BTDriver_Tests_Property_String: RVS_BTDriver_Property {
     override init() {
         super.init()
         rawValue = "Open the pod bay doors, Hal.".data(using: .utf8)
@@ -418,7 +418,7 @@ class RVS_BTDriver_iOS_Tests_Property_String: RVS_BTDriver_Property {
 /* ###################################################################################################################################### */
 /**
  */
-class RVS_BTDriver_iOS_Tests_Property_StringJapanese: RVS_BTDriver_Property {
+class RVS_BTDriver_Tests_Property_StringJapanese: RVS_BTDriver_Property {
     override init() {
         super.init()
         rawValue = "ポッドベイドア、Halを開きます。".data(using: .utf8)
