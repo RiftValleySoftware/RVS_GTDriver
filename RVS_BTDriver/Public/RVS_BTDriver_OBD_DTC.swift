047e7c6ddc8b15c5debc4f354ea8f6e2e8ac37d7
Chris Marshall <chris@riftvalleysoftware.com>
2020-01-24 12:41:34 -0500

    Set up a test skeleton, and commented up the current DTC struct.

--- RVS_BTDriver/Public/RVS_BTDriver_OBD_DTC.swift
+++ RVS_BTDriver/Public/RVS_BTDriver_OBD_DTC.swift
@@ -29,12 +29,16 @@ import Foundation
  */
 public struct RVS_BTDriver_OBD_DTC {
     /* ################################################################## */
-    /**
+    /**he code, itself, is stored as a two-byte integer
      */
     private let _code: UInt16
     
     /* ################################################################## */
     /**
+     This function will read the integer code, and return a "[P|U|C|B]****" string
+     
+     - parameter inCode: The code (an unsigned, 16-bit integer).
+     - returns: A String, in the DTC format (Alphabetic chracater -U, P, C or B, followed by four hex digits). Can be nil, if the code cannot be translated.
      */
     private static func _convertCodeToString(_ inCode: UInt16) -> String! {
         return nil
@@ -42,13 +46,18 @@ public struct RVS_BTDriver_OBD_DTC {
     
     /* ################################################################## */
     /**
+     This function reads the hex string returned by the device, and renders it into the UInt16 necessary for use as the code.
+     
+     - parameter inStringData: The String data, raw from the command.
+     - returns: The rendered code, as a UInt16.
      */
-    private static func _convertStringToCode(_ inCode: String) -> UInt16! {
+    private static func _convertStringToCode(_ inStringData: String) -> UInt16! {
         return nil
     }
     
     /* ################################################################## */
     /**
+     This returns the code, in its "native" form.
      */
     var intValue: UInt16 {
         return _code
@@ -56,6 +65,7 @@ public struct RVS_BTDriver_OBD_DTC {
     
     /* ################################################################## */
     /**
+     This returns the code, in its alphanumeric (familiar) form.
      */
     var stringValue: String {
         return Self._convertCodeToString(_code)
@@ -63,6 +73,7 @@ public struct RVS_BTDriver_OBD_DTC {
     
     /* ################################################################## */
     /**
+     Initialize with a UInt16 code.
      */
     init(code inCode: UInt16) {
         _code = inCode
@@ -70,8 +81,9 @@ public struct RVS_BTDriver_OBD_DTC {
     
     /* ################################################################## */
     /**
+     Intiatlize with raw string data.
      */
-    init(stringData: String) {
-        _code = 0
+    init(stringData inStringData: String) {
+        _code = Self._convertStringToCode(inStringData)
     }
 }
