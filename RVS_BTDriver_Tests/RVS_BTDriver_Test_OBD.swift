613c79dc9e09b22c9fce44f3aff304903d2a8506
Chris Marshall <chris@riftvalleysoftware.com>
2020-02-06 08:46:29 -0500

    I added the rest of the "brutal test" strings to the 0300 test.

--- RVS_BTDriver_Tests/RVS_BTDriver_Test_OBD.swift
+++ RVS_BTDriver_Tests/RVS_BTDriver_Test_OBD.swift
@@ -871,8 +871,22 @@ class RVS_BTDriver_TestParser: XCTestCase {
         // The first String is the actual data that mocks that returned by the device. The second String represents the expected output afte parsing. We treat the instance like an Array of String.
         let testingStrings: [(String, [String])] = [
             ("0300\nSEARCHING...\n43 01 01 08\n\n>", ["P0108"]),
-            ("0300\n43 02 01 08 02 32", ["P0108", "P0232"])
+            ("0300\n43 02 01 08 02 32", ["P0108", "P0232"]),
+            ("0300\n008\n0: 43 03 01 08 02 32\n1: 0C 87 00 00 00 00 00\n\n>", ["P0108", "P0232", "P0C87"]),
+            ("0300\n00C\n0: 43 05 01 08 02 32\n1: 0C 87 00 01 0A 01 00\n\n>", ["P0001", "P0108", "P0232", "P0A01", "P0C87"]),
+            ("0300\n00E\n0: 43 06 01 08 02 32\n1: 0C 87 00 01 0A 01 0B\n2: 00 00 00 00 00 00 00\n\n>", ["P0001", "P0108", "P0232", "P0A01", "P0B00", "P0C87"]),
+            ("0300\n43 01 0C 5A", ["P0C5A"]),
+            ("0300\n43 02 0C 5A 05 37", ["P0537", "P0C5A"]),
+            ("0300\n4GHI3020JKLC5AMNOPQRS0537TUVWXYZ", ["P0537", "P0C5A"]),
+            ("0300\n008\n0: 43 03 0C 5A 05 37\n1: 04 94 00 00 00 00 00", ["P0494", "P0537", "P0C5A"]),
+            ("0300\n00A\n0: 43 04 0C 5A 05 37\n1: 04 94 00 9E 00 00 00", ["P009E", "P0494", "P0537", "P0C5A"]),
+            ("0300\n00C\n0: 43 05 0C 5A 05 37\n1: 04 94 00 9E 00 10 00\n\n>", ["P0010", "P009E", "P0494", "P0537", "P0C5A"]),
+            ("0300\n00E\n0: 43 06 0C 5A 05 37\n1: 04 94 00 9E 00 10 00\n2: 11 00 00 00 00 00 00", ["P0010", "P0011", "P009E", "P0494", "P0537", "P0C5A"]),
+            ("0300\n018\n0: 43 06 0C 5A 05 37\n1: 04 94 00 9E 00 10 00\n2: 11 01 08 02 32 0C 87\n3: 00 01 0A 01 00 00 00", ["P0001", "P0010", "P0011", "P009E", "P0108", "P0232", "P0494", "P0537", "P0A01", "P0C5A", "P0C87"]),
+            ("0300\n01C\n0: 43 06 4C 5A 05 37\n1: 04 94 40 9E 00 10 00\n2387651: 11 01 08 02 32 0C 87\n3465: C0 01 CA 01 FF 01 3F\n4: 01 FF FF FF FF FF FF", ["P0010", "P0011", "P0108", "P0232", "P0494", "P0537", "P0C87", "P3F01", "C009E", "C0C5A", "U0001", "U0A01", "U3F01"]),
+            ("0300\nSEARCHING...\n024\n0: 43 06 4C 5A 05 37\n1: 04 94 40 9E 00 10 00-OOHTHISLOOKSTOUGH\n2387651: 11 01 08 02 32 0C 87\n3465: C0 01 CA 01 FF 01 3F\n4: 01 0C 5A FF FF 04 94\nSUPERCALIFRAGILISTICEXPIALIDOTIOUS:00 10 FF FF FF FF FF:IMALITTLETEAPOT-BLOODYANDCUT\nTHIS-IS-MY-HANDLE\nTHIS:IS-MY-BUTT\n\n>", ["P0010", "P0011", "P0108", "P0232", "P0494", "P0537", "P0C5A", "P0C87", "P3F01", "C009E", "C0C5A", "U0001", "U0A01", "U3F01", "U3FFF"])
         ]
+        
         for testSet in testingStrings {
             let rawResponseDataString1 = testSet.0
             let rawResponseData1 = rawResponseDataString1.data(using: .utf8)
